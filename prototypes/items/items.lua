38d06642ba9db01c180578d76d88c12d5186d5f6
pyanodon <smylodon@gmail.com>
2018-06-29 21:44:05 -0300

    test

--- prototypes/items/items.lua
+++ prototypes/items/items.lua
@@ -1,20 +1,10 @@
 ITEM {
     type = "item",
-    name = "quartz",
+    name = "quartz-ore",
     icon = "__pyrawores__/graphics/icons/ores/quartz.png",
 	icon_size = 32,
     flags = {"goes-to-main-inventory"},
     subgroup = "py-rawores-ores",
     order = "a",
     stack_size = 500
-}
-ITEM {
-    type = "item",
-    name = "powdered-quartz",
-    icon = "__pyrawores__/graphics/icons/ores/powdered-quartz.png",
-	icon_size = 32,
-    flags = {"goes-to-main-inventory"},
-    subgroup = "py-rawores-ores",
-    order = "a",
-    stack_size = 200
 }
