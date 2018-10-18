2b680341570fc4a7e1337cd609269690a1df4aa8
pyanodon <smylodon@gmail.com>
2018-10-18 18:27:45 -0300

    updates
    Former-commit-id: 9e5d7073ce2539c8ad7e2c0e5f08f49eedaedd19
    Former-commit-id: b776d9354d7d544c664817191c8479e35aa8f396

---
+++ prototypes/technologies/quartz-1.lua
@@ -0,0 +1,16 @@
+TECHNOLOGY {
+    type = "technology",
+    name = "quartz-1",
+    icon = "__pyhightech__/graphics/technology/quartz-1.png",
+    icon_size = 128,
+    order = "c-a",
+    prerequisites = {"mining-machines-1"},
+    effects = {},
+    unit = {
+        count = 50,
+        ingredients = {
+            {"science-pack-1", 1},
+        },
+        time = 45
+    }
+}
