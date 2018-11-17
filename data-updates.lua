af65b3e154be73ab163365ccdc0523cbec594d0a
pyanodon <smylodon@gmail.com>
2018-11-16 22:37:12 -0200

    added-distilator
    Former-commit-id: b9f8386ab0c32f8338d7d0d77b25768c5cd64ade
    Former-commit-id: 5c982182edfc1388dcdcf6693fbf15b77b2de130

--- data-updates.lua
+++ data-updates.lua
@@ -27,3 +27,7 @@ RECIPE('py-roboport-mk01'):replace_ingredient('iron-plate', 'aluminium-plate')
 RECIPE('py-recharge-station-mk01'):add_ingredient({type = "item", name = "aluminium-plate", amount =10})
 RECIPE('quartz-mine'):add_ingredient({type = "item", name = "py-construction-robot-01", amount =10})
 
+RECIPE('distilator-mk04'):add_ingredient({type = "item", name = "accumulator-mk01", amount = 2})
+
+
+RECIPE('processing-unit'):add_ingredient({type = "item", name = "gold-plate", amount = 1})
