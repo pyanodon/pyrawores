cb1acfba49ac6078a39260caead97237df84ce40
pyanodon <smylodon@gmail.com>
2018-11-22 20:03:50 -0200

    Titanium-mine

--- prototypes/buildings/tin-mine.lua
+++ prototypes/buildings/tin-mine.lua
@@ -204,8 +204,8 @@ ENTITY {
     },
     vehicle_impact_sound = {filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65},
     working_sound = {
-        sound = {filename = "__pyrawores__/sounds/titanium-mine.ogg", volume = 1.1},
-        idle_sound = {filename = "__pyrawores__/sounds/titanium-mine.ogg", volume = 0.7},
+        sound = {filename = "__pyrawores__/sounds/tin-mine.ogg", volume = 1.1},
+        idle_sound = {filename = "__pyrawores__/sounds/tin-mine.ogg", volume = 0.7},
         apparent_volume = 2.5
     }
 }
