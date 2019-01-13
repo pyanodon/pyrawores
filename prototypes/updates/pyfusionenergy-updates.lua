a4b5de983f5eda3a89caf8d8f7d44db8fed2a91c
pyanodon <smylodon@gmail.com>
2019-01-13 13:01:01 -0200

    fix air

--- prototypes/updates/pyfusionenergy-updates.lua
+++ prototypes/updates/pyfusionenergy-updates.lua
@@ -8,12 +8,7 @@ RECIPE("olefin"):remove_unlock("fuel-production"):add_unlock("machines-mk02")
 RECIPE("gasoline"):remove_unlock("fuel-production"):add_unlock("machines-mk02")
 RECIPE("pressured-air"):set_fields {energy_required = 1}
 
-if not mods["pyhightech"] then
-    RECIPE('pressured-air'):remove_unlock('helium-processing'):add_unlock('coal-processing-1')
-else
-    RECIPE('pressured-air'):remove_unlock('semiconductor-doping'):add_unlock('coal-processing-1')
-end
-
+RECIPE('pressured-air'):remove_unlock('helium-processing'):add_unlock('coal-processing-1')
 
 RECIPE("steam-heating"):remove_ingredient("fuelrod-mk01"):add_ingredient({type = "item", name = "fuelrod-mk01", amount = 1})
 RECIPE("hydrocyclone"):replace_ingredient("centrifuge", "classifier")
