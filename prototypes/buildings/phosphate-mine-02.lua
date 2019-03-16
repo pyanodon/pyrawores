be417fabbf09ab9c57cbf4d8dad22984f21a839b
pyanodon <smylodon@gmail.com>
2019-03-15 22:15:56 -0300

    pollution

--- prototypes/buildings/phosphate-mine-02.lua
+++ prototypes/buildings/phosphate-mine-02.lua
@@ -53,7 +53,7 @@ ENTITY {
       fuel_category = "mega-drill-head",
       effectivity = 1,
       fuel_inventory_size = 1,
-      emissions = 0.1,
+      emissions = 0.001,
     },
     energy_usage = "2000kW",
     mining_power = 6,
--- prototypes/buildings/quartz-mine.lua
+++ prototypes/buildings/quartz-mine.lua
@@ -65,7 +65,7 @@ ENTITY {
       fuel_category = "mega-drill-head",
       effectivity = 1,
       fuel_inventory_size = 1,
-      emissions = 0.1 / 3,
+      emissions = 0.001,
     },
     energy_usage = "2000kW",
     mining_power = 6,
