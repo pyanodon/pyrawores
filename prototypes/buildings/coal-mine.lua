fb53b77951671e608d04bb342ee76dd1af91e287
kingarthur91 <arthurkoy_durbin@yahoo.com>
2019-04-20 22:39:05 -0400

    lowered pullotion back down to a reasonable amount
    Former-commit-id: 3c46ca2288b7f3043dfc2095583c952e92725aed
    Former-commit-id: 10f3c34467d252620b4ba5dd8337d1c704f018c8

--- prototypes/buildings/coal-mine.lua
+++ prototypes/buildings/coal-mine.lua
@@ -66,7 +66,7 @@ ENTITY {
       fuel_category = "mega-drill-head",
       effectivity = 1,
       fuel_inventory_size = 1,
-      emissions_per_minute = 50,
+      emissions_per_minute = 0.06,
       smoke =
       {
         {
