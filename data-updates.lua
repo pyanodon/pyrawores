ae4d9a34878c3e6fbf1e925cdab082ecd614112f
Christian Walde <walde.christian@gmail.com>
2019-05-03 10:54:36 +0200

    naive fix for the player -> character rename

--- data-updates.lua
+++ data-updates.lua
@@ -167,8 +167,8 @@ data.raw.item["wood"].stack_size = 500
 --data.raw.item["raw-wood"].stack_size = 500
 
 local default = setmetatable({}, require("__stdlib__/stdlib/utils/classes/string_array"))
-for _, player in DATA:pairs('player') do
-    player.crafting_categories = player:get_field('crafting_categories', default) + 'handcrafting'
+for _, character in DATA:pairs('character') do
+    character.crafting_categories = character:get_field('crafting_categories', default) + 'handcrafting'
 end
 for _, controller in DATA:pairs('god-controller') do
     controller.crafting_categories = controller:get_field('crafting_categories', default) + 'handcrafting'
