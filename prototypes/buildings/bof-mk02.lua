2ee018533a28799003342737dae138742194b16c
pyanodon <smylodon@gmail.com>
2019-09-01 13:05:29 -0300

    bofs

--- prototypes/buildings/bof-mk02.lua
+++ prototypes/buildings/bof-mk02.lua
@@ -33,7 +33,7 @@ ENTITY {
     icon = "__pyrawores__/graphics/icons/bof-mk02.png",
 	icon_size = 32,
     flags = {"placeable-neutral", "player-creation"},
-    minable = {mining_time = 1, result = "bof-mk02"},
+    minable = {mining_time = 0.5, result = "bof-mk02"},
     fast_replaceable_group = "bof-mk01",
     max_health = 100,
     corpse = "medium-remnants",
