39b265b824ccfdb857f4fd427ea506d58546c605
kingarthur91 <arthurkoy_durbin@yahoo.com>
2019-11-23 05:36:34 -0500

    fixes

--- prototypes/ores/copper.lua
+++ prototypes/ores/copper.lua
@@ -9,37 +9,37 @@ data.raw.resource['copper-ore'].minable.results =
 		{type = "item", name = "ore-copper-trace", amount_min = 1, amount_max = 1, probability = 0.05}
 	}
 
-data.raw.resource['ore-copper-medium'] = table.deepcopy(data.raw.resource['copper-ore'])
-data.raw.resource['ore-copper-medium'].name = 'ore-copper-medium'
-data.raw.resource['ore-copper-medium'].minable.results =
+data.raw.resource['copper-ore-medium'] = table.deepcopy(data.raw.resource['copper-ore'])
+data.raw.resource['copper-ore-medium'].name = 'ore-copper-medium'
+data.raw.resource['copper-ore-medium'].minable.results =
 	{
 		{type = "item", name = "ore-copper", amount_min = 1, amount_max = 1, probability = 0.4},
 		{type = "item", name = "ore-copper-medium", amount_min = 1, amount_max = 1, probability = 0.4},
 		{type = "item", name = "ore-copper-low", amount_min = 1, amount_max = 1, probability = 0.175},
 		{type = "item", name = "ore-copper-trace", amount_min = 1, amount_max = 1, probability = 0.025}
 	}
-data.raw.resource['ore-copper-medium'].autoplace = {}
+data.raw.resource['copper-ore-medium'].autoplace = {}
 
-data.raw.resource['ore-copper-low'] = table.deepcopy(data.raw.resource['copper-ore'])
-data.raw.resource['ore-copper-low'].name = 'ore-copper-low'
-data.raw.resource['ore-copper-low'].minable.results =
+data.raw.resource['copper-ore-low'] = table.deepcopy(data.raw.resource['copper-ore'])
+data.raw.resource['copper-ore-low'].name = 'ore-copper-low'
+data.raw.resource['copper-ore-low'].minable.results =
 	{
 		{type = "item", name = "ore-copper", amount_min = 1, amount_max = 1, probability = 0.15},
 		{type = "item", name = "ore-copper-medium", amount_min = 1, amount_max = 1, probability = 0.2},
 		{type = "item", name = "ore-copper-low", amount_min = 1, amount_max = 1, probability = 0.5},
 		{type = "item", name = "ore-copper-trace", amount_min = 1, amount_max = 1, probability = 0.15}
 	}
-data.raw.resource['ore-copper-low'].autoplace = {}
+data.raw.resource['copper-ore-low'].autoplace = {}
 
-data.raw.resource['ore-copper-trace'] = table.deepcopy(data.raw.resource['copper-ore'])
-data.raw.resource['ore-copper-trace'].name = 'ore-copper-trace'
-data.raw.resource['ore-copper-trace'].minable.results =
+data.raw.resource['copper-ore-trace'] = table.deepcopy(data.raw.resource['copper-ore'])
+data.raw.resource['copper-ore-trace'].name = 'ore-copper-trace'
+data.raw.resource['copper-ore-trace'].minable.results =
 	{
 		{type = "item", name = "ore-copper", amount_min = 1, amount_max = 1, probability = 0.05},
 		{type = "item", name = "ore-copper-medium", amount_min = 1, amount_max = 1, probability = 0.2},
 		{type = "item", name = "ore-copper-low", amount_min = 1, amount_max = 1, probability = 0.3},
 		{type = "item", name = "ore-copper-trace", amount_min = 1, amount_max = 1, probability = 0.45}
 	}
-data.raw.resource['ore-copper-trace'].autoplace = {}
+data.raw.resource['copper-ore-trace'].autoplace = {}
 
 end
--- prototypes/ores/iron.lua
+++ prototypes/ores/iron.lua
@@ -9,20 +9,20 @@ data.raw.resource['iron-ore'].minable.results =
 		{type = "item", name = "ore-iron-trace", amount_min = 1, amount_max = 1, probability = 0.025}
 	}
 
-data.raw.resource['ore-iron-medium'] = table.deepcopy(data.raw.resource['iron-ore'])
-data.raw.resource['ore-iron-medium'].name = 'ore-iron-medium'
-data.raw.resource['ore-iron-medium'].minable.results =
+data.raw.resource['iron-ore-medium'] = table.deepcopy(data.raw.resource['iron-ore'])
+data.raw.resource['iron-ore-medium'].name = 'iron-ore-medium'
+data.raw.resource['iron-ore-medium'].minable.results =
 	{
 		{type = "item", name = "ore-iron", amount_min = 1, amount_max = 1, probability = 0.4},
 		{type = "item", name = "ore-iron-medium", amount_min = 1, amount_max = 1, probability = 0.4},
 		{type = "item", name = "ore-iron-low", amount_min = 1, amount_max = 1, probability = 0.175},
 		{type = "item", name = "ore-iron-trace", amount_min = 1, amount_max = 1, probability = 0.025}
 	}
-data.raw.resource['ore-iron-medium'].autoplace = {}
+data.raw.resource['iron-ore-medium'].autoplace = {}
 
-data.raw.resource['ore-iron-low'] = table.deepcopy(data.raw.resource['iron-ore'])
-data.raw.resource['ore-iron-low'].name = 'ore-iron-low'
-data.raw.resource['ore-iron-low'].minable.results =
+data.raw.resource['iron-ore-low'] = table.deepcopy(data.raw.resource['iron-ore'])
+data.raw.resource['iron-ore-low'].name = 'iron-ore-low'
+data.raw.resource['iron-ore-low'].minable.results =
 	{
 		{type = "item", name = "ore-iron", amount_min = 1, amount_max = 1, probability = 0.15},
 		{type = "item", name = "ore-iron-medium", amount_min = 1, amount_max = 1, probability = 0.2},
@@ -31,15 +31,15 @@ data.raw.resource['ore-iron-low'].minable.results =
 	}
 data.raw.resource['ore-iron-low'].autoplace = {}
 
-data.raw.resource['ore-iron-trace'] = table.deepcopy(data.raw.resource['iron-ore'])
-data.raw.resource['ore-iron-trace'].name = 'ore-iron-trace'
-data.raw.resource['ore-iron-trace'].minable.results =
+data.raw.resource['iron-ore-trace'] = table.deepcopy(data.raw.resource['iron-ore'])
+data.raw.resource['iron-ore-trace'].name = 'iron-ore-trace'
+data.raw.resource['iron-ore-trace'].minable.results =
 	{
 		{type = "item", name = "ore-iron", amount_min = 1, amount_max = 1, probability = 0.05},
 		{type = "item", name = "ore-iron-medium", amount_min = 1, amount_max = 1, probability = 0.2},
 		{type = "item", name = "ore-iron-low", amount_min = 1, amount_max = 1, probability = 0.3},
 		{type = "item", name = "ore-iron-trace", amount_min = 1, amount_max = 1, probability = 0.45}
 	}
-data.raw.resource['ore-iron-trace'].autoplace = {}
+data.raw.resource['iron-ore-trace'].autoplace = {}
 
 end
