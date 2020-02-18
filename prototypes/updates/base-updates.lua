46bb2ed55fda1c00112234ebcfa4fc06e4ee5fe0
kingarthur91 <arthurkoy_durbin@yahoo.com>
2020-02-18 05:24:46 -0500

    working on coal removeal
    Former-commit-id: 2a9d3a87e9d9164b3dc66db08bb8925ee990fbae
    Former-commit-id: 63e5e3229a07b3789fa04d6059bc43694e9342f6

--- prototypes/updates/base-updates.lua
+++ prototypes/updates/base-updates.lua
@@ -1,2 +1,5 @@
 
 RECIPE('atomic-bomb'):remove_ingredient('uranium-235'):add_ingredient({type = "item", name = "yellow-cake", amount = 5})
+
+data.raw.resource['coal'].autoplace = nil
+--data.raw.resource['coal'] = nil
