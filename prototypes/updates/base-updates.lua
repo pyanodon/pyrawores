a14683db574115d4100b236d7b5735cd59a2ee3b
kingarthur91 <arthurkoy_durbin@yahoo.com>
2020-04-11 06:09:28 -0400

    removed coal from new game menu

--- prototypes/updates/base-updates.lua
+++ prototypes/updates/base-updates.lua
@@ -2,4 +2,10 @@
 RECIPE('atomic-bomb'):remove_ingredient('uranium-235'):add_ingredient({type = "item", name = "yellow-cake", amount = 5})
 
 data.raw.resource['coal'].autoplace = nil
---data.raw.resource['coal'] = nil
+data.raw['autoplace-control']['coal'] = nil
+
+for _, preset in pairs(data.raw["map-gen-presets"]["default"]) do
+    if preset and preset.basic_settings and preset.basic_settings.autoplace_controls and preset.basic_settings.autoplace_controls['coal'] then
+      preset.basic_settings.autoplace_controls['coal'] = nil
+    end
+end
