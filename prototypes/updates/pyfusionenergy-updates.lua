71aff9cf72e7598232f6f25eec774e0ac5749c3b
pyanodon <smylodon@gmail.com>
2020-04-19 13:08:27 -0300

    fix pyfe
    Former-commit-id: e1b34f96a95a68d9337106ed9fbc5514d5fc0b43
    Former-commit-id: a979ec9f8fd98fe4bff08455ff25d86703e4de5e

--- prototypes/updates/pyfusionenergy-updates.lua
+++ prototypes/updates/pyfusionenergy-updates.lua
@@ -9,6 +9,8 @@ RECIPE("olefin"):remove_unlock("fuel-production"):add_unlock("machines-mk02")
 RECIPE("gasoline"):remove_unlock("fuel-production"):add_unlock("machines-mk02")
 RECIPE("pressured-air"):set_fields {energy_required = 1}
 
+ITEM("molybdenum-plate").stack_size = 500
+
 --RECIPE('pressured-air'):remove_unlock('helium-processing'):add_unlock('coal-processing-1')
 
 RECIPE("steam-heating"):remove_ingredient("fuelrod-mk01"):add_ingredient({type = "item", name = "fuelrod-mk01", amount = 1})
