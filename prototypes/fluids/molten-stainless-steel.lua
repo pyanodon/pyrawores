e581c9f10d3f80336a602f5b79b722184bbc0018
pyanodon <smylodon@gmail.com>
2020-06-25 20:23:48 -0300

    icons

--- prototypes/fluids/molten-stainless-steel.lua
+++ prototypes/fluids/molten-stainless-steel.lua
@@ -17,7 +17,7 @@ FLUID {
 FLUID {
     type = "fluid",
     name = "molten-stainless-steel-p1",
-    icon = "__pyraworesgraphics__/graphics/icons/molten-stainless-steel.png",
+    icon = "__pyraworesgraphics__/graphics/icons/molten-stainless-steel-p1.png",
 	icon_size = 32,
     default_temperature = 10, -- less than 15 = liquid / equal a 15 = gas
     base_color = {r = 0.5, g = 0.5, b = 0.6},
@@ -33,7 +33,7 @@ FLUID {
 FLUID {
     type = "fluid",
     name = "molten-stainless-steel-p2",
-    icon = "__pyraworesgraphics__/graphics/icons/molten-stainless-steel.png",
+    icon = "__pyraworesgraphics__/graphics/icons/molten-stainless-steel-p2.png",
 	icon_size = 32,
     default_temperature = 10, -- less than 15 = liquid / equal a 15 = gas
     base_color = {r = 0.5, g = 0.5, b = 0.6},
@@ -49,7 +49,7 @@ FLUID {
 FLUID {
     type = "fluid",
     name = "molten-stainless-steel-p3",
-    icon = "__pyraworesgraphics__/graphics/icons/molten-stainless-steel.png",
+    icon = "__pyraworesgraphics__/graphics/icons/molten-stainless-steel-p3.png",
 	icon_size = 32,
     default_temperature = 10, -- less than 15 = liquid / equal a 15 = gas
     base_color = {r = 0.5, g = 0.5, b = 0.6},
