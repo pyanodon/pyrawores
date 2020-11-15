faa716f3fd4305611cfdf1ea85fbc0bc18445d58
pyanodon <smylodon@gmail.com>
2020-11-15 09:27:23 -0300

    mold

--- prototypes/items/items.lua
+++ prototypes/items/items.lua
@@ -2032,3 +2032,24 @@ ITEM {
     order = 'b',
     stack_size = 500
 }
+
+ITEM {
+    type = 'item',
+    name = 'mold',
+    icon = '__pyraworesgraphics__/graphics/icons/mip/mold-1.png',
+    icon_size = 64,
+    icon_mipmaps = 7,
+    pictures = {
+        {size = 64, filename = '__pyraworesgraphics__/graphics/icons/mip/mold-1.png', scale = 0.25, mipmap_count = 1},
+        {size = 64, filename = '__pyraworesgraphics__/graphics/icons/mip/mold-2.png', scale = 0.25, mipmap_count = 1},
+        {size = 64, filename = '__pyraworesgraphics__/graphics/icons/mip/mold-3.png', scale = 0.25, mipmap_count = 1},
+        {size = 64, filename = '__pyraworesgraphics__/graphics/icons/mip/mold-4.png', scale = 0.25, mipmap_count = 1},
+        {size = 64, filename = '__pyraworesgraphics__/graphics/icons/mip/mold-5.png', scale = 0.25, mipmap_count = 1},
+        {size = 64, filename = '__pyraworesgraphics__/graphics/icons/mip/mold-6.png', scale = 0.25, mipmap_count = 1},
+        {size = 64, filename = '__pyraworesgraphics__/graphics/icons/mip/mold-7.png', scale = 0.25, mipmap_count = 1},
+    },
+    flags = {},
+    subgroup = "py-rawores-ores",
+    order = "aaa",
+    stack_size = 500
+}
