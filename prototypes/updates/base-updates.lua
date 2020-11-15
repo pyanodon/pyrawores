637dfe70c830a6c44ff725ca70ce12eea0ecba93
pyanodon <smylodon@gmail.com>
2020-11-15 09:49:26 -0300

    electric

--- prototypes/updates/base-updates.lua
+++ prototypes/updates/base-updates.lua
@@ -1,5 +1,6 @@
 
 RECIPE('atomic-bomb'):remove_ingredient('uranium-235'):add_ingredient({type = "item", name = "yellow-cake", amount = 5})
+RECIPE('electric-engine-unit'):add_ingredient({type = "item", name = "iron-stick", amount = 1}):add_ingredient({type = "item", name = "copper-cable", amount = 300}):add_ingredient({type = "item", name = "aluminium-plate", amount = 10}):add_ingredient({type = "item", name = "bolts", amount = 20}):remove_ingredient("engine-unit")
 
 data.raw.reactor['nuclear-reactor'].use_fuel_glow_color = true
 data.raw.item['uranium-fuel-cell'].fuel_glow_color = {0,1,0,1}
