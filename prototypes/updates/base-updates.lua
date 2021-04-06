be94b1848b5903508a0d2bba75f895aa1087a456
pyanodon <smylodon@gmail.com>
2021-04-05 22:00:18 -0300

    balance

--- prototypes/updates/base-updates.lua
+++ prototypes/updates/base-updates.lua
@@ -1,6 +1,6 @@
 
 RECIPE('atomic-bomb'):remove_ingredient('uranium-235'):add_ingredient({type = "item", name = "yellow-cake", amount = 5})
-RECIPE('electric-engine-unit'):add_ingredient({type = "item", name = "iron-stick", amount = 1}):add_ingredient({type = "item", name = "copper-cable", amount = 300}):add_ingredient({type = "item", name = "aluminium-plate", amount = 10}):add_ingredient({type = "item", name = "bolts", amount = 20}):remove_ingredient("engine-unit")
+RECIPE('electric-engine-unit'):add_ingredient({type = "item", name = "iron-stick", amount = 1}):add_ingredient({type = "item", name = "copper-cable", amount = 150}):add_ingredient({type = "item", name = "aluminium-plate", amount = 10}):add_ingredient({type = "item", name = "bolts", amount = 20}):remove_ingredient("engine-unit")
 
 RECIPE('flying-robot-frame'):add_unlock('robotics')
 RECIPE('low-density-structure'):change_category('py-rawores-smelter')
