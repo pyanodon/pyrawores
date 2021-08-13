ae53e7c89627569750ddd36cd08d528af157e3e7
kingarthur91 <arthurkoy_durbin@yahoo.com>
2021-08-13 05:31:29 -0400

    removing unnecessary vanilla uranium recipes and techs

--- prototypes/updates/base-updates.lua
+++ prototypes/updates/base-updates.lua
@@ -1,5 +1,18 @@
 
+--adjusting base uranium and nuclear
 RECIPE('atomic-bomb'):remove_ingredient('uranium-235'):add_ingredient({type = "item", name = "yellow-cake", amount = 5})
+
+RECIPE("uranium-processing"):remove_unlock('uranium-processing')
+
+TECHNOLOGY("uranium-processing"):set_field{enabled = false}
+
+RECIPE("kovarex-enrichment-process"):remove_unlock('kovarex-enrichment-process')
+
+TECHNOLOGY("kovarex-enrichment-process"):set_field{enabled = false}
+TECHNOLOGY("utility-science-pack"):remove_prereq('kovarex-enrichment-process')
+
+RECIPE("nuclear-fuel"):remove_unlock('kovarex-enrichment-process'):add_unlock('uranium-mk04')
+
 RECIPE('electric-engine-unit'):remove_ingredient('copper-cable'):add_ingredient({type = "item", name = "copper-cable", amount = 150}):add_ingredient({type = "item", name = "aluminium-plate", amount = 10}):add_ingredient({type = "item", name = "bolts", amount = 20})--:remove_ingredient("engine-unit")
 
 RECIPE('flying-robot-frame'):add_unlock('robotics')
