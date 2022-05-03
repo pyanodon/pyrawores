72989eaccfe7d5b48b1bf37039a0933e98470456
ShadowGlass0 <gkecskem@gmail.com>
2022-05-03 22:38:43 +1000

    Tech tree fixes (#60)

--- prototypes/updates/pycoalprocessing-updates.lua
+++ prototypes/updates/pycoalprocessing-updates.lua
@@ -42,7 +42,7 @@ RECIPE('nexelit-plate'):remove_unlock('nexelit'):set_fields{hidden = true}
 
 --Autofactory
 
-RECIPE('automated-factory-mk01'):replace_ingredient('advanced-circuit', 'electronic-circuit'):remove_unlock('advanced-electronics'):add_unlock('automation-2')
+RECIPE('automated-factory-mk01'):replace_ingredient('advanced-circuit', 'electronic-circuit'):remove_unlock('advanced-electronics'):add_unlock('fast-inserter')
 
 --advanced-circuit
 
