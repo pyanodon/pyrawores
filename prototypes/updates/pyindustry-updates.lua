37e84b2175aac420d034bdcea4f159490aad996f
ShadowGlass0 <gkecskem@gmail.com>
2022-05-31 01:22:38 +1000

    Update radars tech (#72)

--- prototypes/updates/pyindustry-updates.lua
+++ prototypes/updates/pyindustry-updates.lua
@@ -1,7 +1,11 @@
 -- TECH CHANGES
 TECHNOLOGY("railway-mk01"):add_prereq("solder-mk01")
 TECHNOLOGY("railway-mk02"):remove_prereq("nexelit"):add_prereq("nexelit-mk01")
+TECHNOLOGY("radars-mk01"):remove_pack("logistic-science-pack")
 
+if mods["pyhightech"] then
+    TECHNOLOGY("radars-mk02"):remove_pack("chemical-science-pack")
+end
 
 -- RECIPE UNLOCKS
 
