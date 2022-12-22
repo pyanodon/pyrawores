59905c72380e2dffe6b6dc0d9e6d1b829a25148f
kingarthur91 <arthurkoy_durbin@yahoo.com>
2022-12-22 01:36:37 -0500

    you want the plates. YOU CANT HANDLE THE PLATES

--- prototypes/recipes/recipes-solder.lua
+++ prototypes/recipes/recipes-solder.lua
@@ -32,6 +32,7 @@ RECIPE {
     order = "j"
 }:add_unlock("solder-mk02")
 
+--[[
 RECIPE {
     type = "recipe",
     name = "solder-1",
@@ -90,3 +91,83 @@ RECIPE {
     subgroup = "py-rawores-solder",
     order = "j"
 }:add_unlock("solder-mk03")
+]]--
+
+--SOLDER
+
+RECIPE {
+    type = "recipe",
+    name = "molten-solder-0",
+    category = "bof",
+    enabled = false,
+    energy_required = 4,
+    ingredients = {
+        {type = "fluid", name = "molten-tin", amount = 10},
+        {type = "fluid", name = "molten-lead", amount = 20},
+    },
+    results = {
+        {type = "fluid", name = "molten-solder", amount = 20}
+    },
+    main_product = 'molten-solder',
+    subgroup = "py-rawores-solder",
+    order = "j"
+}:add_unlock("solder-mk02")
+
+RECIPE {
+    type = "recipe",
+    name = "molten-solder-1",
+    category = "bof",
+    enabled = false,
+    energy_required = 2,
+    ingredients = {
+        {type = "fluid", name = "molten-tin", amount = 10},
+        {type = "fluid", name = "molten-lead", amount = 20},
+        {type = "fluid", name = "tar", amount = 30},
+    },
+    results = {
+        {type = "fluid", name = "molten-solder", amount = 40}
+    },
+    main_product = 'molten-solder',
+    subgroup = "py-rawores-solder",
+    order = "j"
+}:add_unlock("solder-mk02")
+
+RECIPE {
+    type = "recipe",
+    name = "molten-solder-2",
+    category = "bof",
+    enabled = false,
+    energy_required = 2,
+    ingredients = {
+        {type = "fluid", name = "molten-tin", amount = 30},
+        {type = "fluid", name = "molten-copper", amount = 10},
+        {type = "fluid", name = "molten-silver", amount = 10},
+        {type = "fluid", name = "tar", amount = 30},
+    },
+    results = {
+        {type = "fluid", name = "molten-solder", amount = 70}
+    },
+    main_product = 'molten-solder',
+    subgroup = "py-rawores-solder",
+    order = "j"
+}:add_unlock("solder-mk03")
+
+RECIPE {
+    type = "recipe",
+    name = "molten-solder-3",
+    category = "bof",
+    enabled = false,
+    energy_required = 2,
+    ingredients = {
+        {type = "fluid", name = "molten-tin", amount = 20},
+        {type = "fluid", name = "molten-lead", amount = 50},
+        {type = "fluid", name = "molten-silver", amount = 10},
+        {type = "fluid", name = "tar", amount = 50},
+    },
+    results = {
+        {type = "fluid", name = "molten-solder", amount = 140}
+    },
+    main_product = 'molten-solder',
+    subgroup = "py-rawores-solder",
+    order = "j"
+}:add_unlock("solder-mk04")
