1e83b82afae436af72c4b232725420f845f3312a
JStMorgan <jake.adam.morgan1994@gmail.com>
2023-05-06 09:23:23 +0200

    Added Salt Mine circuit connections

---
+++ prototypes/circuit-connector-definitions-pyro.lua
@@ -0,0 +1,10 @@
+salt_mine_connector_definitions = circuit_connector_definitions.create
+(
+  universal_connector_template,
+  {--Directions are up, right, down, left.
+    { variation = 25, main_offset = util.by_pixel(135, -105), shadow_offset = util.by_pixel(145, -100), show_shadow = false }, 
+    { variation = 25, main_offset = util.by_pixel(135, -105), shadow_offset = util.by_pixel(145, -100), show_shadow = false },
+    { variation = 25, main_offset = util.by_pixel(135, -105), shadow_offset = util.by_pixel(145, -100), show_shadow = false },
+    { variation = 25, main_offset = util.by_pixel(135, -105), shadow_offset = util.by_pixel(145, -100), show_shadow = false }
+  }
+)
