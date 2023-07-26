274e9ce954550a9bcaa05f820eeb9893ed6c8638
kingarthur91 <arthurkoy_durbin@yahoo.com>
2023-07-26 00:07:17 -0400

    Revert "purest nitrogen"
    This reverts commit ac1d19b631f45ff50d069a832eb71af4253089e1.

--- prototypes/fluids/purest-nitrogen-gas.lua
+++ prototypes/fluids/purest-nitrogen-gas.lua
@@ -10,12 +10,9 @@ RECIPE {
         {type = "fluid", name = "nitrogen", amount = 20},
         {type = "fluid", name = "oxygen", amount = 20}
     },
-    localised_name = {"recipe-name.fractional-distillation"},
-    localised_description = {"recipe-description.fractional-distillation"},
+    main_product = "purest-nitrogen-gas",
     subgroup = "py-rawores-fluids",
-    order = "dab",
-    icon = "__pyraworesgraphics__/graphics/icons/fractional-distillation.png",
-    icon_size = 32
+    order = "dab"
 }:add_unlock("nitrogen-mk01")
 
 FLUID {
@@ -31,20 +28,3 @@ FLUID {
     subgroup = "py-rawores-fluids",
     order = "c"
 }
-
-if mods["pyfusionenergy"] then
-    RECIPE {
-        type = "recipe",
-        name = "purest-nitrogen-gas-recycle",
-        category = "vacuum",
-        enabled = false,
-        energy_required = 1,
-        ingredients = {
-            {type = "fluid", name = "purest-nitrogen-gas", amount = 60}
-        },
-        results = {
-            {type = "fluid", name = "pressured-air", amount = 60}
-        },
-        main_product = "pressured-air"
-    }:add_unlock("nitrogen-mk01")
-end
