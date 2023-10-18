a8ee94d0921bf7329d4466e8b28b888a995d7776
notnotmelon <zacharypicco@gmail.com>
2023-10-18 16:40:01 -0500

    https://github.com/pyanodon/pybugreports/issues/319

--- prototypes/fluids/nexelit-slurry.lua
+++ prototypes/fluids/nexelit-slurry.lua
@@ -3,7 +3,7 @@ FLUID {
     name = "nexelit-slurry",
     icon = "__pyraworesgraphics__/graphics/icons/nexelit-slurry.png",
 	icon_size = 32,
-    default_temperature = 15, -- less than 15 = liquid / equal a 15 = gas
+    default_temperature = 10, -- less than 15 = liquid / equal a 15 = gas
     base_color = {r = 0, g = 0.545, b = 0.733},
     flow_color = {r = 1, g = 1, b = 1},
     max_temperature = 100,
