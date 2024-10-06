ceffca4b08cda60d75a05038da7651adf90129c7
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2024-10-07 00:51:19 +0200

    use lib on_nth_tick

--- control.lua
+++ control.lua
@@ -2,4 +2,11 @@ require '__pypostprocessing__.lib'
 
 require 'scripts/wiki/text-pages'
 
+remote.add_interface("pyro", {
+    ---@param func string
+    execute_on_nth_tick = function(func)
+        py.mod_nth_tick_funcs[func]()
+    end
+})
+
 py.finalize_events()
