460b39fc495fdfc227e17657a8c5cef545f305f2
Zachary Picco <zacharypicco@gmail.com>
2025-04-03 14:29:16 -0500

    Added new icons for molten silver. Resolves https://github.com/pyanodon/pybugreports/issues/943

--- prototypes/fluids/molten-silver.lua
+++ prototypes/fluids/molten-silver.lua
@@ -1,8 +1,8 @@
 FLUID {
     type = "fluid",
     name = "molten-silver",
-    icon = "__pyraworesgraphics__/graphics/icons/molten-chromium.png",
-    icon_size = 32,
+    icon = "__pyraworesgraphics__/graphics/icons/molten-silver.png",
+    icon_size = 64,
     default_temperature = 10, -- less than 15 = liquid / equal a 15 = gas
     base_color = {r = 0.7, g = 0.7, b = 0.7},
     flow_color = {r = 1, g = 1, b = 1},
