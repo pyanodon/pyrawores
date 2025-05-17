e31661da240190cfbefea2a8cced74efc24e77b7
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2025-05-17 23:42:19 +0200

    unhide functions from the profiler

--- control.lua
+++ control.lua
@@ -2,11 +2,11 @@ require "__pypostprocessing__.lib"
 
 require "scripts/wiki/text-pages"
 
-remote.add_interface("pyro", {
-    ---@param func string
-    execute_on_nth_tick = function(func)
+py.on_event(defines.events.on_tick, function(event)
+    local func_list = remote.call("on_nth_tick", "query", "pyro", event.tick)
+    for _, func in pairs(func_list) do
         py.mod_nth_tick_funcs[func]()
     end
-})
+end)
 
 py.finalize_events()
