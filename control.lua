4bae8464287f97eb490c8800bd80f7ac3460e104
lambdalemon <lemonjameslem@protonmail.com>
2025-08-29 22:28:45 -0400

    more formatting

--- control.lua
+++ control.lua
@@ -5,7 +5,7 @@ require("scripts/wiki/text-pages")
 py.on_event(defines.events.on_tick, function(event)
     local func_list = remote.call("on_nth_tick", "query", "pyro", event.tick)
     for _, func in pairs(func_list) do
-        py.mod_nth_tick_funcs[ func ]()
+        py.mod_nth_tick_funcs[func]()
     end
 end)
 
